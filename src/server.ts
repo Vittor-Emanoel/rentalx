99ae1c4db9a75870e98edcfbd934e3166ef0b6fe
Vittor-Emanoel <vittoremanoel1234@gmail.com>
2023-09-14 22:11:34 -0300

    feat: create categories route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from 'routes/categories.routes';
 
 const app = express();
+
+app.use(express.json());
+app.use(categoriesRoutes);
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello world' });
